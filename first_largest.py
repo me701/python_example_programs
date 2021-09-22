e987369a3d9114cbf6753dd9f82bf98c264e93bd
Benjamin Weinhold <bweinhold@ksu.edu>
2021-09-22 14:46:02 -0500

    used enumerate

--- first_largest.py
+++ first_largest.py
@@ -7,9 +7,13 @@ def largest_element(a, loc = False):
     """
     maxval = a[0]
     location = 0
-    for i in range(1,len(a)):
-        if a[i] > maxval:
-            maxval = a[i]
+    # for i in range(1,len(a)):
+    #     if a[i] > maxval:
+    #         maxval = a[i]
+    #         location = i
+    for (i,e) in enumerate(a):
+        if e > maxval:
+            maxval = e
             location = i
     if loc == True:
         return maxval, location
