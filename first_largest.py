8ab59154f46dc9acc7638fbb7d3eb5824d11655a
Benjamin Weinhold <bweinhold@ksu.edu>
2021-09-24 12:16:45 -0500

    added exception error handling

--- first_largest.py
+++ first_largest.py
@@ -5,23 +5,26 @@ Python program to find the largest element and its location.
 def largest_element(a, loc = False):
     """ Return the largest element of a sequence a.
     """
-    maxval = a[0]
-    location = 0
-    # for i in range(1,len(a)):
-    #     if a[i] > maxval:
-    #         maxval = a[i]
-    #         location = i
-    for (i,e) in enumerate(a):
-        if e > maxval:
-            maxval = e
-            location = i
-    if loc == True:
-        return maxval, location
-    else:
-        return maxval
+    try:
+        maxval = a[0]
+        location = 0
+        for (i,e) in enumerate(a):
+            if e > maxval:
+                maxval = e
+                location = i
+        if loc == True:
+            return maxval, location
+        else:
+            return maxval
+    except ValueError:
+        return "Value Error"
+    except TypeError:
+        return "Type error, my dude. You can't compare those."
+    except:
+        return "Unforseen error! What did you do?"
 
 
 if __name__ == "__main__":
 
-    a = [1,2,3,2,1]
+    a = ["a","b","c",2,1]
     print("Largest element is {:}".format(largest_element(a, loc=True)))
